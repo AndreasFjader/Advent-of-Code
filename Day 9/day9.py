6fd591b3dbc9d6cb3fb29a2321630a77df094787
Andreas Fjäder <andreas.fjader@quinyx.com>
2023-12-09 22:40:18 +0100

    Day 9 code update

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -2,10 +2,9 @@ t1 = 0
 t2 = 0
 for value in open('in.txt').read().splitlines():
     seq = [int(v) for v in value.split()]
-    last = [seq[-1]]
-    first = [seq[0]]
+    first, last = [seq[0]], [seq[-1]]
     
-    while not all([s == 0 for s in seq]):
+    while not all(s == 0 for s in seq):
         seq2 = []
         for i in range(0, len(seq) - 1):
             seq2.append(seq[i + 1] - seq[i])
